62cd271232a1bc0a37f402ff363cc9a0b4a9ae32
liveless Academy <givebackacademie@gmail.com>
2025-12-31 00:54:55 +0100

    initial

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Starting Ant Media Server..."
-exec /usr/local/antmedia/start.sh
-stat /usr/local/antmedia/start.sh
+exec: "/usr/local/antmedia/start.sh"
+stat: "/usr/local/antmedia/start.sh"
 
